ffce12fa50861d28229a008f29120ea0315653ab
Bala <Bala@DESKTOP-5IGHGQV>
2020-04-08 11:21:00 +0530

    First Commit in eclipse

---
+++ loosu/src/Love/NewTest.java
@@ -0,0 +1,14 @@
+package Love;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  
+  public void f() {
+	  
+	  DesiredCapabilities cap = new DesiredCapabilities();
+	  
+  }
+}
